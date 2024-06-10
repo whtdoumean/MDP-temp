58172b86b95273b1e0297c44e513a0ca9b9660b7
Iliya <74312039+whtdoumean@users.noreply.github.com>
2024-06-10 15:58:16 +0300

    Add files via upload

---
+++ data/story.js
@@ -0,0 +1,48 @@
+var story = {
+ "docName": "MDP",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": false,
+ "highlightAllHotspots": false,
+ "hideGallery": true,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "MDP",
+ "layersExist": false,
+ "pages": [
+  {
+   "id": "4006:7",
+   "groupIndex": 0,
+   "title": "Desktop - 21",
+   "image": "desktop-21.png",
+   "index": 0,
+   "width": 1920,
+   "height": 3366,
+   "x": 1038,
+   "y": -2541,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null,
+   "tmpSolitBackground": false
+  }
+ ],
+ "totalImages": 1,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#1E1E1E"
+  }
+ ]
+}
